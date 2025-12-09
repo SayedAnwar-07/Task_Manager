e7276ff293a529c89c2c3bd2d13d44f542f15711
SayedAnwarOfficial <sayedanwarofficial@gmail.com>
2025-12-10 00:40:45 +0600

    CSS colour change and navbar fixed

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -27,17 +27,23 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <AuthProvider>
           <ThemeProvider>
             <div className="flex min-h-screen flex-col">
-              <header className="border-b">
+
+              <header className="fixed top-0 left-0 w-full z-50 border-b bg-white dark:bg-[#101010]">
                 <div className="max-w-5xl mx-auto flex items-center justify-between px-4 py-3">
                   <Link href="/" className="font-semibold flex gap-2 items-center">
-                    <img src="https://i.ibb.co.com/ds9rs0YX/image-removebg-preview.png" alt="logo" className="w-10 h-10"/>
+                    <img
+                      src="https://i.ibb.co.com/ds9rs0YX/image-removebg-preview.png"
+                      alt="logo"
+                      className="w-10 h-10"
+                    />
                     <div className="font-light text-sm">
-                      <p className="font-semibold">Eentra <span className="dark:text-[#e44332] text-[#2b564e] font-bold">BD</span></p>
+                      <p className="font-semibold">
+                        Eentra <span className="dark:text-[#e44332] text-[#2b564e] font-bold">BD</span>
+                      </p>
                       <p>Task Manager</p>
                     </div>
                   </Link>
 
-                  {/* CLIENT COMPONENT CAN USE useAuth() */}
                   <div className="flex gap-3">
                     <ThemeToggle />
                     <Navbar />
@@ -45,7 +51,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
                 </div>
               </header>
 
-              <main className="max-w-6xl mx-auto w-full px-4 py-6">
+              <main className="max-w-6xl mx-auto w-full px-4 py-6 pt-[100px]">
                 {children}
                 <Toaster />
               </main>
